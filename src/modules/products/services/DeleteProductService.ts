7d15dc4d6f68c29e1d8ca9a010ce6d18b52d86d8
Julio Alves <julioasf@gmail.com>
2021-07-08 17:20:31 -0300

    Seçao 5 - Capitulos 28-32.

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,23 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string; // string porque o id é do tipo uuid
+}
+
+class DeleteProductService {
+  public async execute({ id }: IRequest): Promise<void> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+
+    await productsRepository.remove(product); // ao invés de se utilizar o id, está sendo usada a entidade/objeto product
+  }
+}
+
+export default DeleteProductService;
