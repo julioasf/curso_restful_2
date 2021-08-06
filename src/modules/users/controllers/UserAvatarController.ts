12486c9b4860311e2695ba4dc23d94e7cf85dada
Julio Alves <julioasf@gmail.com>
2021-08-06 17:06:18 -0300

    Seçao 7 - Capitulos 63-70.

--- src/modules/users/controllers/UserAvatarController.ts
+++ src/modules/users/controllers/UserAvatarController.ts
@@ -5,6 +5,14 @@ export default class UserAvatarController {
   public async update(request: Request, response: Response): Promise<Response> {
     const updateAvatar = new UpdateUserAvatarService();
 
+    //////////////////////////////////////////////////
+    console.log(request);
+    //console.log(request.body);
+    //console.log(request.file.filename);
+    //console.log(request.file);
+    return response.json({});
+    //////////////////////////////////////////////////
+
     const user = updateAvatar.execute({
       user_id: request.user.id,
       avatarFileName: request.file.filename,
